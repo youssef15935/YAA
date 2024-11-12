865795ef1d1de38ef49f5b261f19f1cca222da7c
Youssef Abdeljalil <youssefpaintingtv@gmail.com>
2024-11-12 16:37:36 +0100

    added some dependencies

--- src/architect-backend/config/db.js
+++ src/architect-backend/config/db.js
@@ -1,14 +1,17 @@
+// src/architect-backend/config/db.js
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true, // Gardez seulement cette option
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
     });
-    console.log('MongoDB connecté');
-  } catch (error) {
-    console.error(error.message);
-    process.exit(1);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('Error connecting to MongoDB', err);
+    process.exit(1); // Arrêter le processus si la connexion échoue
   }
 };
 
